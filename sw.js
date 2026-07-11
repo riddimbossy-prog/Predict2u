2b7d95a1916584317465aa39df4032942ac6f56b
riddimbossy-prog <riddimbossy@gmail.com>
2026-07-11 03:39:03 +0000

    ui fix

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Static assets: cache-first with background refresh.
    - Cache entries are versioned. Bump CACHE_VERSION when releasing changes. */
 
-const CACHE_VERSION = "predict2u-v153-logo1";
+const CACHE_VERSION = "predict2u-v154";
 const OFFLINE_PAGE = "./board.html";
 
 const SHELL = [
